2db302595796ddf1d3e76259098ed8d4c91b1354
milmilkim <soundkim27@gmail.com>
2023-09-27 20:02:37 +0900

    fix: 이사감

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import CommonLayout from './components/layout/CommonLayout';
 
 import Home from './pages/Home';
@@ -7,40 +7,34 @@ import Event from './pages/Event';
 import Film from './pages/Film';
 import Ticket from './pages/Ticket';
 
-const router = createHashRouter(
-  [
-    {
-      path: '/',
-      element: <CommonLayout />,
-      children: [
-        {
-          path: '',
-          element: <Home />,
-        },
-        {
-          path: '/about',
-          element: <About />,
-        },
-        {
-          path: '/event',
-          element: <Event />,
-        },
-        {
-          path: '/film',
-          element: <Film />,
-        },
-        {
-          path: '/ticket',
-          element: <Ticket />,
-        },
-      ],
-    },
-  ],
+const router = createBrowserRouter([
   {
-    basename:
-    import.meta.env.MODE === 'production' ? `https://milmilkim.github.io/project-merge/` : '/',
-  }
-);
+    path: '/',
+    element: <CommonLayout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/event',
+        element: <Event />,
+      },
+      {
+        path: '/film',
+        element: <Film />,
+      },
+      {
+        path: '/ticket',
+        element: <Ticket />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
   },
-  base: process.env.NODE_ENV === 'production' ? `https://milmilkim.github.io/project-merge/` : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
